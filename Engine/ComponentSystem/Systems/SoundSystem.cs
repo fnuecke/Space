728334cd9e086cd584303e0246c18e7bedf5fd9d
lord joda <lordjoda@gmail.com>
2011-12-20 00:02:54 +0100

    sound playing and timeout and stuff
    Former-commit-id: 8a224a6817715e4c20f190af3ea15b4105134131

--- Engine/ComponentSystem/Systems/SoundSystem.cs
+++ Engine/ComponentSystem/Systems/SoundSystem.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using Engine.ComponentSystem.Parameterizations;
 using Engine.Math;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 
 namespace Engine.ComponentSystem.Systems
 {
@@ -16,9 +18,22 @@ namespace Engine.ComponentSystem.Systems
         /// A list of sounds to play in this update.
         /// </summary>
         private List<SoundParameterization> _sounds = new List<SoundParameterization>();
+        private Dictionary<string ,int > playedSounds = new Dictionary<string, int>();
 
+        /// <summary>
+        /// Time in which the sound shall not be played again
+        /// </summary>
+        private const int Cooldown = 5;
+        protected SoundBank SoundBank;
         #endregion
 
+
+        #region Constructor
+        public SoundSystem(SoundBank soundBank)
+        {
+            SoundBank = soundBank;
+        }
+        #endregion
         #region Logic
 
         public override void Update(ComponentSystemUpdateType updateType)
@@ -39,15 +54,63 @@ namespace Engine.ComponentSystem.Systems
             }
             else if (updateType == ComponentSystemUpdateType.Display)
             {
+
+
+                foreach (var key in playedSounds.Keys)
+                {
+                    if (--playedSounds[key] == 0)
+                        playedSounds.Remove(key);
+                    
+                }
                 // Get position of listener, i.e. what the sounds are relative to.
                 FPoint listenerPosition = GetListenerPosition();
+                FPoint listenerVelocity = GetListenerVelocity();
+
+                var listener = new AudioListener();
+                //Listener
+                //Position
+                Vector3 positionListener;
+                positionListener.X = (float)listenerPosition.X;
+                positionListener.Y = (float)listenerPosition.Y;
+                positionListener.Z = 0;
+                listener.Position = positionListener;
+                //Velocity
+                Vector3 velocityListener;
+                velocityListener.X = (float)listenerVelocity.X;
+                velocityListener.Y = (float)listenerVelocity.Y;
+                velocityListener.Z = 0;
+                listener.Velocity = velocityListener;
 
                 // Actually play the sounds that should be this update.
                 foreach (var sound in _sounds)
                 {
-                    // TODO play
-                    // TODO only play sounds again after a certain timeout (50ms or so) if XACT doesn't do that itself (which it probably doesn't?)
+                    //sound was not played in the last clicks
+                    if (!playedSounds.ContainsKey(sound.SoundCueToPlay))
+                    {
+                        playedSounds.Add(sound.SoundCueToPlay,Cooldown);
+                        var emitter = new AudioEmitter();
+                        
+
+                        //Emitter
+
+                        //Velocity
+                        Vector3 velocity;
+                        velocity.X = (float)sound.Velocity.X;
+                        velocity.Y = (float)sound.Velocity.Y;
+                        velocity.Z = 0;
+                        emitter.Velocity = velocity;
+                        
+                        //Position
+                        Vector3 positionEmitter;
+                        positionEmitter.X = (float)sound.Position.X;
+                        positionEmitter.Y = (float)sound.Position.Y;
+                        positionEmitter.Z = 0;
+                        emitter.Position = positionEmitter;
+                        SoundBank.PlayCue(sound.SoundCueToPlay,listener,emitter);
+                    }
+                    
                 }
+
             }
         }
 
@@ -59,7 +122,14 @@ namespace Engine.ComponentSystem.Systems
         {
             return FPoint.Zero;
         }
-
+        /// <summary>
+        /// Get the position of the listener (e.g. player avatar).
+        /// </summary>
+        /// <returns></returns>
+        protected FPoint GetListenerVelocity()
+        {
+            return FPoint.Zero;
+        }
         #endregion
 
         #region Cloning
