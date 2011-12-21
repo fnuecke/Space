aefe224f4dcc4aa892fd74794cc324bded3c32a3
lordjoda <lordjoda@gmail.com>
2011-12-21 22:26:30 +0100

    sound
    Former-commit-id: acc806488bdf33d8b5568f5dab6fd0fd551f1383

---
+++ Space/ComponentSystem/Systems/PlayerCenteredSoundSystem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine.ComponentSystem.Systems;
+using Engine.Math;
+using Microsoft.Xna.Framework.Audio;
+using Engine.Session;
+using Engine.ComponentSystem.Components;
+
+namespace Space.ComponentSystem.Systems
+{
+    class PlayerCenteredSoundSystem : SoundSystem
+    {
+        #region Fields
+        IClientSession _session;
+        #endregion
+        public PlayerCenteredSoundSystem(SoundBank soundbank, IClientSession session)
+            :base(soundbank)
+        {
+            _session = session;
+        }
+        protected override FPoint GetListenerPosition()
+        {
+            var avatar = Manager.GetSystem<AvatarSystem>().GetAvatar(_session.LocalPlayer.Number);
+            if (avatar != null)
+            {
+                return avatar.GetComponent<Transform>().Translation;
+            }
+            return FPoint.Zero;
+        }
+
+        protected override FPoint GetListenerVelocity()
+        {
+            var avatar = Manager.GetSystem<AvatarSystem>().GetAvatar(_session.LocalPlayer.Number);
+            if (avatar != null)
+            {
+                return avatar.GetComponent<Velocity>().Value;
+            }
+            return FPoint.Zero;
+        }
+    }
+}
