cf67e186e59407236a7cb2170c0594e3c43e0bf8
Florian Nücke <florian@nuecke.de>
2012-01-06 15:29:50 +0100

    rename
    Former-commit-id: 0a38011305dc46e2a35aee817eb1a7308ab1abc0

---
+++ SpaceData/Modules/SensorModule.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Engine.Data;
+using Engine.Serialization;
+using Engine.Util;
+
+namespace Space.Data.Modules
+{
+    public class SensorModule : AbstractEntityModule<EntityAttributeType>
+    {
+        #region Fields
+
+        /// <summary>
+        /// The Range this Radar has
+        /// </summary>
+        public float Range;
+
+        #endregion
+
+        #region Constructor
+
+        public SensorModule()
+        {
+            AddAttributeTypeToInvalidate(EntityAttributeType.SensorRange);
+        }
+
+        #endregion
+
+        #region Serialization / Hashing / Cloning
+
+        public override Packet Packetize(Packet packet)
+        {
+            base.Packetize(packet)
+                .Write(Range);
+
+            return packet;
+        }
+
+        public override void Depacketize(Packet packet)
+        {
+            base.Depacketize(packet);
+
+            Range = packet.ReadSingle();
+        }
+
+        public override void Hash(Hasher hasher)
+        {
+            base.Hash(hasher);
+
+            hasher.Put(BitConverter.GetBytes(Range));
+        }
+
+        #endregion
+    }
+}
