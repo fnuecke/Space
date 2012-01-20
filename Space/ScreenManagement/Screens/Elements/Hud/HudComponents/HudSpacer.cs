6b11789e19edaf94ee2c9b8f3b5397f81853f584
Daniel Kaltenthaler <dakatotal@googlemail.com>
2012-01-20 17:14:41 +0100

    Documentation only
    Former-commit-id: 606b0e22d02ce726d47e188c3e12443d76f4ef1d

--- Space/ScreenManagement/Screens/Elements/Hud/HudComponents/HudSpacer.cs
+++ Space/ScreenManagement/Screens/Elements/Hud/HudComponents/HudSpacer.cs
@@ -15,18 +15,49 @@ namespace Space.ScreenManagement.Screens.Elements.Hud
     class HudSpacer : IHudChildElement
     {
 
+        #region Constants
+
+        /// <summary>
+        /// Enums for the different Modes that are possible within this class:
+        /// </summary>
         public enum Mode
         {
             Top, Center, Bottom
         }
 
+        /// <summary>
+        /// Standard value for the width.
+        /// </summary>
         private const int StandardWidth = 302;
+
+        /// <summary>
+        /// Standard value for the thickness of the spacer.
+        /// </summary>
         private const int StandardThicknessSpacer = 2;
+
+        /// <summary>
+        /// Standard value for the thickness of the line on the side.
+        /// </summary>
         private const int StandardThicknessLineSide = 1;
+
+        /// <summary>
+        /// Standard value for the length of the line on the side.
+        /// </summary>
         private const int StandardLengthLineSide = 8;
+
+        /// <summary>
+        /// Standard value for the size of the outter border.
+        /// </summary>
         private const int StandardOutterBorder = 1;
+
+        /// <summary>
+        /// Standard value for the size of the inner border.
+        /// </summary>
         private const int StandardInnerBorder = 3;
 
+        #endregion
+
+        #region Fields
 
         /// <summary>
         /// The local client, used to fetch player's position and radar range.
@@ -48,28 +79,84 @@ namespace Space.ScreenManagement.Screens.Elements.Hud
         /// </summary>
         private BasicForms _basicForms;
 
+        /// <summary>
+        /// The height of the object (addition of thickness spacer + outter border + inner border)
+        /// </summary>
         private int _height;
+
+        /// <summary>
+        /// The current mode.
+        /// </summary>
         private Mode _mode;
 
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The width of the element.
+        /// </summary>
         public int Width { get; set; }
+
+        /// <summary>
+        /// The thickness of the spacer.
+        /// </summary>
         public int ThicknessSpacer { get; set; }
+
+        /// <summary>
+        /// The thickness of the lines on the side.
+        /// </summary>
         public int ThicknessLineSide { get; set; }
+
+        /// <summary>
+        /// The length of the lines on the side.
+        /// </summary>
         public int LengthLineSide { get; set; }
-        public Point Position { get; set; }
+
+        /// <summary>
+        /// The size of the outter border
+        /// </summary>
         public int OutterBorder { get; set; }
+
+        /// <summary>
+        /// The size of the inner border
+        /// </summary>
         public int InnerBorder { get; set; }
 
+        // Implementation of IHudChildElement interface
+        public Point Position { get; set; }
+
+        #endregion
+
+        #region Getter
+
+        /// <summary>
+        /// Calculates the height of the element and returns the value.
+        /// The height is an addition of the outer border, the thickniss of the
+        /// spacer and the inner border.
+        /// The lines on the side will NOT be added to the height.
+        /// </summary>
+        /// <returns>The height of this element.</returns>
         public int GetHeight()
         {
             return OutterBorder + ThicknessSpacer + InnerBorder;
         }
 
+        #endregion
 
+        #region Initialization
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="client">The general client object.</param>
+        /// <param name="mode">The mode that this object should use.</param>
         public HudSpacer(GameClient client, Mode mode)
         {
             _client = client;
             _mode = mode;
 
+            // set the standard values into the field.
             Width = StandardWidth;
             _height = ThicknessSpacer + StandardOutterBorder + StandardInnerBorder;
             OutterBorder = StandardOutterBorder;
@@ -89,6 +176,10 @@ namespace Space.ScreenManagement.Screens.Elements.Hud
             _spriteBatch = spriteBatch;
             _basicForms = new BasicForms(_spriteBatch, _client);
         }
+
+        #endregion
+
+        #region Draw
 
         /// <summary>
         /// Render the HUD header with the current values.
@@ -140,5 +231,8 @@ namespace Space.ScreenManagement.Screens.Elements.Hud
             _spriteBatch.End();
 
         }
+
+        #endregion
+
     }
 }
