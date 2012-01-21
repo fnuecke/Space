444804a3b53e28eafa91366a4f2f499a174a9154
Florian Nücke <florian@nuecke.de>
2012-01-21 03:40:55 +0100

    explosion on death (seems delayed, though, needs checking)
    Former-commit-id: 64c81a79b7b3b452b0ca8405dac93db49d87152e

--- Space/ComponentSystem/Components/ExplosionEffect.cs
+++ Space/ComponentSystem/Components/ExplosionEffect.cs
@@ -1,24 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Space.ComponentSystem.Components
+﻿namespace Space.ComponentSystem.Components
 {
-    class ExplosionEffect:Effect
+    sealed class ExplosionEffect : Effect
     {
         public ExplosionEffect()
-            :base("Effects/BasicExplosion")
+            : base("Effects/BasicExplosion")
         {
-            DrawOrder = 40;
         }
 
         public override void Update(object parameterization)
         {
             base.Update(parameterization);
-            if (_effect[0] != null && _isDrawingInstance)
+            if (_effect[0] != null)
             {
-                Emitting = false;
+                if (Emitting && _effect[0].ActiveParticlesCount > 0)
+                {
+                    Emitting = false;
+                }
+                else if (!Emitting && _effect[0].ActiveParticlesCount == 0)
+                {
+                    Entity.Manager.RemoveEntity(Entity);
+                }
             }
         }
     }
