f6d91085ae3aa0f45eb78881fb1588f8ef09a520
Daniel Kaltenthaler <dakatotal@googlemail.com>
2012-01-22 20:29:25 +0100

    Separate hud labels
    Former-commit-id: 28ceeeeab66073ec61a95838c2603ff86a2eaa97

---
+++ Space/ScreenManagement/Screens/Elements/Hud/HudComponents/HudSingleLabel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Space.ScreenManagement.Screens.Elements.Hud.HudComponents
+{
+    class HudSingleLabel
+    {
+
+        public HudSingleLabel()
+        {
+ 
+        }
+
+
+    }
+}
