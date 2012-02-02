3c01fd7247ea04fadf3f2375c65fe86b76d10361
Daniel Kaltenthaler <dakatotal@googlemail.com>
2012-02-02 03:35:26 +0100

    Abstract input handling methods now check itself if input handling is allowed for the element. To implement input actions override the methods DoHandleXXX() instead of HandleXXX() now.
    Former-commit-id: 5e88610debf81f8508f855b18f3b304d59a1d886

--- Space/ScreenManagement/Screens/Ingame/Interfaces/AbstractGuiElement.cs
+++ Space/ScreenManagement/Screens/Ingame/Interfaces/AbstractGuiElement.cs
@@ -25,6 +25,11 @@ namespace Space.ScreenManagement.Screens.Ingame.Interfaces
         /// </summary>
         protected readonly GameClient _client;
 
+        /// <summary>
+        /// Holds all GUI elements that are child elements of this element.
+        /// </summary>
+        private List<AbstractGuiElement> _childElements;
+
         #endregion
 
         #region Properties
@@ -49,7 +54,8 @@ namespace Space.ScreenManagement.Screens.Ingame.Interfaces
         /// <param name="client">The local client.</param>
         public AbstractGuiElement(GameClient client)
         {
-            this._client = client;
+            _client = client;
+            _childElements = new List<AbstractGuiElement>();
 
             Enabled = false;
             Visible = false;
@@ -78,41 +84,269 @@ namespace Space.ScreenManagement.Screens.Ingame.Interfaces
 
         #endregion
 
-        #region Mouse, Keyboard and Gameplay Listener
+        #region Listener Basics (not overrideable)
 
-        public virtual bool HandleKeyPressed(Keys key)
+        /// <summary>
+        /// Handles the key pressed event.
+        /// Returns false if the GUI element does not allow input handling.
+        /// 
+        /// Cannot be overwritten. For handling specific user inputs please 
+        /// overwrite the method DoHandleKeyPressed.
+        /// </summary>
+        /// <returns>True if the element is allowed to handle inputs, False else.</returns>
+        public bool HandleKeyPressed(Keys key)
         {
-            return false;
+            // return false if this element is not allowed to handling inputs
+            if (!IsInputHandlingAllowed())
+            {
+                return false;
+            }
+
+            DoHandleKeyPressed(key);
+
+            // iterate all child elements and return true afterwards
+            foreach (AbstractGuiElement e in _childElements)
+            {
+                e.HandleKeyPressed(key);
+            }
+            return true;
         }
 
-        public virtual bool HandleKeyReleased(Keys key)
+        /// <summary>
+        /// Handles the key released event.
+        /// Returns false if the GUI element does not allow input handling.
+        /// 
+        /// Cannot be overwritten. For handling specific user inputs please 
+        /// overwrite the method DoHandleKeyReleased.
+        /// </summary>
+        /// <returns>True if the element is allowed to handle inputs, False else.</returns>
+        public bool HandleKeyReleased(Keys key)
         {
-            return false;
+            // return false if this element is not allowed to handling inputs
+            if (!IsInputHandlingAllowed())
+            {
+                return false;
+            }
+
+            DoHandleKeyReleased(key);
+
+            // iterate all child elements and return true afterwards
+            foreach (AbstractGuiElement e in _childElements)
+            {
+                e.HandleKeyReleased(key);
+            }
+            return true;
         }
 
-        public virtual bool HandleMousePressed(MouseButtons buttons)
+        /// <summary>
+        /// Handles the mouse pressed event.
+        /// Returns false if the GUI element does not allow input handling.
+        /// 
+        /// Cannot be overwritten. For handling specific user inputs please 
+        /// overwrite the method DoHandleMousePressed.
+        /// </summary>
+        /// <returns>True if the element is allowed to handle inputs, False else.</returns>
+        public bool HandleMousePressed(MouseButtons buttons)
         {
-            return false;
+            // return false if this element is not allowed to handling inputs
+            if (!IsInputHandlingAllowed())
+            {
+                return false;
+            }
+
+            DoHandleMousePressed(buttons);
+
+            // iterate all child elements and return true afterwards
+            foreach (AbstractGuiElement e in _childElements)
+            {
+                e.HandleMousePressed(buttons);
+            }
+            return true;
         }
 
-        public virtual bool HandleMouseReleased(MouseButtons buttons)
+        /// <summary>
+        /// Handles the mouse released event.
+        /// Returns false if the GUI element does not allow input handling.
+        /// 
+        /// Cannot be overwritten. For handling specific user inputs please 
+        /// overwrite the method DoHandleMouseReleased.
+        /// </summary>
+        /// <returns>True if the element is allowed to handle inputs, False else.</returns>
+        public bool HandleMouseReleased(MouseButtons buttons)
         {
-            return false;
+            // return false if this element is not allowed to handling inputs
+            if (!IsInputHandlingAllowed())
+            {
+                return false;
+            }
+
+            DoHandleMouseReleased(buttons);
+
+            // iterate all child elements and return true afterwards
+            foreach (AbstractGuiElement e in _childElements)
+            {
+                e.HandleMouseReleased(buttons);
+            }
+            return true;
         }
 
-        public virtual bool HandleMouseMoved(float x, float y)
+        /// <summary>
+        /// Handles the mouse moved event.
+        /// Returns false if the GUI element does not allow input handling.
+        /// 
+        /// Cannot be overwritten. For handling specific user inputs please 
+        /// overwrite the method DoHandleMouseMoved.
+        /// </summary>
+        /// <returns>True if the element is allowed to handle inputs, False else.</returns>
+        public bool HandleMouseMoved(float x, float y)
         {
-            return false;
+            // return false if this element is not allowed to handling inputs
+            if (!IsInputHandlingAllowed())
+            {
+                return false;
+            }
+
+            DoHandleMouseMoved(x, y);
+
+            // iterate all child elements and return true afterwards
+            foreach (AbstractGuiElement e in _childElements)
+            {
+                e.HandleMouseMoved(x, y);
+            }
+            return true;
         }
 
-        public virtual bool HandleGamePadPressed(Buttons buttons)
+        /// <summary>
+        /// Handles the gamepad key pressed event.
+        /// Returns false if the GUI element does not allow input handling.
+        /// 
+        /// Cannot be overwritten. For handling specific user inputs please 
+        /// overwrite the method DoHandleGamePadPressed.
+        /// </summary>
+        /// <returns>True if the element is allowed to handle inputs, False else.</returns>
+        public bool HandleGamePadPressed(Buttons buttons)
         {
-            return false;
+            // return false if this element is not allowed to handling inputs
+            if (!IsInputHandlingAllowed())
+            {
+                return false;
+            }
+
+            DoHandleGamePadPressed(buttons);
+
+            // iterate all child elements and return true afterwards
+            foreach (AbstractGuiElement e in _childElements)
+            {
+                e.HandleGamePadPressed(buttons);
+            }
+            return true;
         }
 
-        public virtual bool HandleGamePadReleased(Buttons buttons)
+        /// <summary>
+        /// Handles the gamepad key released event.
+        /// Returns false if the GUI element does not allow input handling.
+        /// 
+        /// Cannot be overwritten. For handling specific user inputs please 
+        /// overwrite the method DoHandleGamePadReleased.
+        /// </summary>
+        /// <returns>True if the element is allowed to handle inputs, False else.</returns>
+        public bool HandleGamePadReleased(Buttons buttons)
         {
-            return false;
+            // return false if this element is not allowed to handling inputs
+            if (!IsInputHandlingAllowed())
+            {
+                return false;
+            }
+
+            DoHandleGamePadReleased(buttons);
+
+            // iterate all child elements and return true afterwards
+            foreach (AbstractGuiElement e in _childElements)
+            {
+                e.HandleGamePadReleased(buttons);
+            }
+            return true;
+        }
+
+        #endregion
+
+        #region Listener Actions (overrideable)
+
+        /// <summary>
+        /// Holds the action that is done when the input handler is active.
+        /// Is doing nothing in default, have to be overridden to add action.
+        /// </summary>
+        /// <param name="key"></param>
+        public virtual void DoHandleKeyPressed(Keys key)
+        {
+        }
+
+        /// <summary>
+        /// Holds the action that is done when the input handler is active.
+        /// Is doing nothing in default, have to be overridden to add action.
+        /// </summary>
+        /// <param name="key"></param>
+        public virtual void DoHandleKeyReleased(Keys key)
+        {
+        }
+
+        /// <summary>
+        /// Holds the action that is done when the input handler is active.
+        /// Is doing nothing in default, have to be overridden to add action.
+        /// </summary>
+        /// <param name="key"></param>
+        public virtual void DoHandleMousePressed(MouseButtons buttons)
+        {
+        }
+
+        /// <summary>
+        /// Holds the action that is done when the input handler is active.
+        /// Is doing nothing in default, have to be overridden to add action.
+        /// </summary>
+        /// <param name="key"></param>
+        public virtual void DoHandleMouseReleased(MouseButtons buttons)
+        {
+        }
+
+        /// <summary>
+        /// Holds the action that is done when the input handler is active.
+        /// Is doing nothing in default, have to be overridden to add action.
+        /// </summary>
+        /// <param name="key"></param>
+        public virtual void DoHandleMouseMoved(float x, float y)
+        {
+        }
+
+        /// <summary>
+        /// Holds the action that is done when the input handler is active.
+        /// Is doing nothing in default, have to be overridden to add action.
+        /// </summary>
+        /// <param name="key"></param>
+        public virtual void DoHandleGamePadPressed(Buttons buttons)
+        {
+        }
+
+        /// <summary>
+        /// Holds the action that is done when the input handler is active.
+        /// Is doing nothing in default, have to be overridden to add action.
+        /// </summary>
+        /// <param name="key"></param>
+        public virtual void DoHandleGamePadReleased(Buttons buttons)
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Checks the status if an user input (keyboard, mouse or gamepad) is
+        /// currently allowed for the GUI element.
+        /// </summary>
+        /// <returns>The status if an user input is allowed for the GUI element.</returns>
+        private bool IsInputHandlingAllowed()
+        {
+            return Enabled;
         }
 
         #endregion
