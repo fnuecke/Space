b5a5124a21f1d61c63dfba1c929cabe2d2e896df
Florian Nücke <florian@nuecke.de>
2012-06-08 13:35:28 +0200

    gif loading for awesomium
    Former-commit-id: e406682f8dbda737b710f9f638ecd6dc613feb5b

---
+++ AwesomiumContentPipeline/GifImporter.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using Microsoft.Xna.Framework.Content.Pipeline;
+
+namespace Awesomium.ContentPipeline
+{
+    /// <summary>
+    /// Importer that keeps files intact. Its only purpose is to allow treating
+    /// files of any format, even if they are handled somewhere else (in our
+    /// case image data for Awesomium), through XNAs content pipeline.
+    /// </summary>
+    [ContentImporter(".gif", DisplayName = "Raw Data Importer", DefaultProcessor = "PassThroughProcessor")]
+    public class GifImporter : ContentImporter<byte[]>
+    {
+        public override byte[] Import(string filename, ContentImporterContext context)
+        {
+            return File.ReadAllBytes(filename);
+        }
+    }
+}
