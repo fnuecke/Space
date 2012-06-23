b277e9c83111bbe4b5fe01928b7bf6d259daed56
Florian Nücke <florian@nuecke.de>
2012-06-23 17:03:31 +0200

    some serialization test base classes
    Former-commit-id: 019f5c8e5389a3476375c2057b4514713ff1f4c4

---
+++ EngineTests/ComponentSystem/Components/AbstractItemSerializationTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Engine.ComponentSystem.RPG.Components;
+
+namespace Engine.Tests.ComponentSystem.Components
+{
+    public abstract class AbstractItemSerializationTest<T> : AbstractComponentSerializationTest<T>
+        where T : Item, new()
+    {
+        /// <summary>
+        /// Returns a list of methods that change a value of an instance so
+        /// that its new hash value should be different.
+        /// </summary>
+        protected override IEnumerable<ValueChanger> GetValueChangers()
+        {
+            return new ValueChanger[]
+                   {
+                       instance => instance.Enabled = !instance.Enabled,
+                       instance => instance.Name += "b",
+                       instance => instance.IconName += "b",
+                   }.Concat(base.GetValueChangers());
+        }
+    }
+}
