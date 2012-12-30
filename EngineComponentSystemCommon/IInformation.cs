3c78183783730c2a44f1ffedc4cb2233b0e7558d
lordjoda <lordjoda@googlemail.com>
2012-12-30 18:51:10 +0100

    commenting
    Former-commit-id: 3d5c4ab7dc9783e7ecfaf1bad4039b505ca7509f

--- EngineComponentSystemCommon/IInformation.cs
+++ EngineComponentSystemCommon/IInformation.cs
@@ -6,12 +6,27 @@ using Microsoft.Xna.Framework;
 
 namespace Space.ComponentSystem.Util
 {
+    /// <summary>
+    /// Interface for Display Information. A Component or System that wants its data to be displayed must implement this Information. Used by InformationDisplaySystem
+    /// </summary>
     public interface IInformation
     {
-
+        /// <summary>
+        /// Returns the text to be displayed in the System
+        /// </summary>
+        /// <returns></returns>
         String[] getDisplayText();
 
-         Color getDisplayColor();
+        /// <summary>
+        /// Returns the Color in which the text shall be displayed
+        /// </summary>
+        /// <returns></returns>
+        Color getDisplayColor();
+
+        /// <summary>
+        /// Returns whether this Information shall be displayed
+        /// </summary>
+        /// <returns></returns>
         bool shallDraw();
     }
 }
